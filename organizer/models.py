4e1ca18bd7337cc09cbe079a328c4c01983ae1b7
swheatley <shaylee1@me.com>
2016-06-21 20:14:22 -0600

    Chapter 4 Templates

--- organizer/models.py
+++ organizer/models.py
@@ -8,7 +8,7 @@ class Tag(models.Model):
     slug = models.SlugField( max_length=31, unique=True, help_text='A label for URL config')
 
     def __str__(self):
-        return self.name
+        return self.name.title()
 
     class Meta:
         ordering = ['name']
--- organizer/views.py
+++ organizer/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+
+    html_output = "<html>"
+
+    html_output += "<head>"
+
+    html_output += "<title>"
+
+    html_output += "Don't Do This!</title>\n"
+
+    html_output += "</head>"
+
+    html_output += "<body>"
+
+
+
+    for tag in tag_list:
+        html_output += " <ul>"
+
+        html_output += " <li>"
+
+        html_output += tag.name.title()
+
+        html_output += "</li>"
+
+        html_output += " </ul>"
+
+        html_output += "</body>"
+        html_output += "</html>"
+
+    return HttpResponse(html_output)
