5a2748c5243228ef5336e23f56aad49755153355
swheatley <shaylee1@me.com>
2016-06-25 23:33:23 -0600

    Chapter four, template tags, organization and code

--- organizer/views.py
+++ organizer/views.py
@@ -1,37 +1,13 @@
 from django.http.response import HttpResponse
-
+from django.template import Context, loader
 from .models import Tag
 
 
 def homepage(request):
     tag_list = Tag.objects.all()
-
-    html_output = "<html>"
-
-    html_output += "<head>"
-
-    html_output += "<title>"
-
-    html_output += "Don't Do This!</title>\n"
-
-    html_output += "</head>"
-
-    html_output += "<body>"
+    template = loader.get_template('organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
 
 
-
-    for tag in tag_list:
-        html_output += " <ul>"
-
-        html_output += " <li>"
-
-        html_output += tag.name.title()
-
-        html_output += "</li>"
-
-        html_output += " </ul>"
-
-        html_output += "</body>"
-        html_output += "</html>"
-
-    return HttpResponse(html_output)
+    return HttpResponse(output)
--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'suorganizer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+            # overrides the DIRS key
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
